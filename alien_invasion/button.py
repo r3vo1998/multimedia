1187bdcf4db857d082232913ebf9de9e0a167581
r3vo <revo84191@gmail.com>
2022-10-01 19:21:38 +0300

    GAME - Alien invasion

---
+++ alien_invasion/button.py
@@ -0,0 +1,25 @@
+import pygame.font
+
+class Button():
+    """ Инициализация кнопки """
+    def __init__(self, ai_settings, screen, msg):
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.width, self.height = 200, 30
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+        self.prep_msg(msg)
+
+    def prep_msg(self, msg):
+        # сообщение в кнопке
+        self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        # рисование кнопки
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
